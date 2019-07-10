1b7207dccdc53bae7583983565fe76db86c70e7f
joshemitsu <63394@studmail.htw-aalen.de>
2019-07-10 16:49:56 +0200

    Initial commit. Footsteps funktionieren (meistens...)

---
+++ Assets/PlayerAudioController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioController : MonoBehaviour
+{
+    public AudioClip[] surfaceSounds;
+
+    enum eSurfaces { GRASS, ROAD, WATER };
+    private eSurfaces lastSurface = eSurfaces.ROAD;
+    private Vector3 lastPos;
+    private bool playerMoving = false;
+    private AudioSource audiosource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPos = gameObject.transform.position;
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        checkPlayerMovement();
+        if(playerMoving)
+        {
+            RaycastHit surfacePlane;
+            string surfaceTag = "";
+
+            if (Physics.Raycast(transform.position, Vector3.down, out surfacePlane))
+            {
+                surfaceTag = surfacePlane.collider.tag;
+
+                //if(surfaceTag != lastSurface)...switchcase
+
+                if (surfaceTag == "grass" && (lastSurface != eSurfaces.GRASS))
+                {
+                    lastSurface = eSurfaces.GRASS;
+                    audiosource.volume = 0.2f;
+                    audiosource.clip = surfaceSounds[0];
+                    Debug.Log("grass!");
+                }
+                else if (surfaceTag == "road" && (lastSurface != eSurfaces.ROAD))
+                {
+                    lastSurface = eSurfaces.ROAD;
+                    audiosource.volume = 0.4f;
+                    audiosource.clip = surfaceSounds[1];
+                    Debug.Log("road!");
+                }
+                else if (surfaceTag == "water" && (lastSurface != eSurfaces.WATER))
+                {
+                    lastSurface = eSurfaces.WATER;
+                    audiosource.volume = 0.5f;
+                    audiosource.clip = surfaceSounds[2];
+                    Debug.Log("water!");
+                }
+            }
+
+            if (!(audiosource.isPlaying))
+            {
+                audiosource.Play();
+            }
+        }
+
+        else
+        {
+            if(audiosource.isPlaying)
+            {
+                Debug.Log("pause!");
+                audiosource.Pause();
+            }
+        }
+
+    }
+
+    private void checkPlayerMovement()
+    {
+        //return (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D));
+
+        /*
+        Vector3 curPos = gameObject.transform.position;
+        if(curPos == lastPos)
+        {
+            return false;
+        }
+        //Debug.Log((curPos - lastPos).magnitude);
+        lastPos = curPos;
+        return true;
+        */
+
+        Vector3 curPos = gameObject.transform.position;
+
+        if ((curPos - lastPos).magnitude <= 0.02f)
+        {
+            playerMoving = false;
+            return;
+        }
+
+        playerMoving = true;
+        lastPos = curPos;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 0.08f;
+
+    private Rigidbody rb;
+    private Camera camera;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        camera = gameObject.GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //Vector3 movement = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, Input.GetAxis("Vertical") * speed);
+        Vector3 movement = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * new Vector3(horizontalMovement * speed, 0.0f, verticalMovement * speed);
+
+        //rb.AddForce(movement * speed);
+        gameObject.transform.Translate(movement);
+    }
+}
