982f646916793740e81210abce3ae08dd6f34619
joshemitsu <63394@studmail.htw-aalen.de>
2019-07-11 16:08:35 +0200

    dog und cat audio, linear rolloff

---
+++ Assets/AnimalAudioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalAudioController : MonoBehaviour
+{
+    private AudioSource audiosource;
+
+    public int delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        StartCoroutine(PlaySoundWithDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator PlaySoundWithDelay()
+    {
+        while(true)
+        {
+            audiosource.PlayOneShot(audiosource.clip);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
